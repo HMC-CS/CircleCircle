fd2271938aabf5120bf988a13c557361971826c1
kateaplin <kaplin@g.hmc.edu>
2013-06-10 15:58:10 -0700

    Config File
    Added the config file. Most things are not yet defined

---
+++ CircleCircleRevolution/Config.h
@@ -0,0 +1,90 @@
+//
+//  Config.h
+//  CircleCircleRevolution
+//
+//  Created by Kathryn Aplin on 6/10/13.
+//  Copyright (c) 2013 Kathryn Aplin. All rights reserved.
+//
+
+#ifndef CircleCircleRevolution_Config_h
+#define CircleCircleRevolution_Config_h
+
+// Overall
+#define gameFont
+
+// Gameplay Constants
+
+
+// Button Titles
+#define toMainMenu @"Menu"
+#define toSelectionScreen @"Game"
+#define toSingleGame @"Single"
+#define toDoubleGame @"Double"
+#define toHighScores @"Score"
+#define toInstructions @"Instructions"
+#define toCredits @"Credits"
+
+// Main Menu Formatting - mm
+#define mmSelectionButtonX
+#define mmSelectionButtonY
+#define mmCreditsButtonX
+#define mmCreditsButtonY
+#define mmInstructionsButtonX
+#define mmInstrucitonsButtonY
+#define mmScoresButtonX
+#define mmScoresButtonY
+
+// Selection Screen Formatting - ss
+#define ssMainMenuButtonX
+#define ssMainMenuButtonY
+#define ssSingleGameButtonX
+#define ssSingleGameButtonY
+#define ssDoubleGameButtonX
+#define ssDoubleGameButtonY
+
+
+// Single Circle Game Formatting - sg
+#define sgPauseButtonX
+#define sgPauseButtonY
+#define sgShipX
+#define sgShipY
+#define sgCircleX
+#define sgCircleY
+#define sgFractionX
+#define sgFractionY
+#define sgLifeMeterX
+#define sgLifeMeterY
+
+// Double Circle Game Formatting - dg
+#define dgPauseButtonX
+#define dgPauseButtonY
+#define dgShipX
+#define dgShipY
+#define dgCircle1X
+#define dgCircle1Y
+#define dgCircle2X
+#define dgCircle2Y
+#define dgFractionX
+#define dgFractionY
+#define dgLifeMeterX
+#define dgLifeMeterY
+
+// Credits Screen Formatting - cs
+#define csMainMenuButtonX
+#define csMainMenubuttonY
+#define csFontSize
+#define csFontPositionX
+#define csFontPositionY
+
+// Instrucitons Screen Formatting - is
+#define isMainMenuButtonX
+#define isMainMenubuttonY
+#define isFontSize
+#define isFontPositionX
+#define isFontPositionY
+
+// Resource File Titles - replace when we have them
+
+
+
+#endif
