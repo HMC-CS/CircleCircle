e3da5e9701e016bc8c953d3a253fa1323f531c8f
Buike Ndefo-Dahl <azbk.nd@gmail.com>
2013-06-26 16:23:43 -0700

    reset high scores to not be stupid

--- CircleCircleRevolution/CircleCircleRevolution/ScoreViewController.m
+++ CircleCircleRevolution/CircleCircleRevolution/ScoreViewController.m
@@ -47,8 +47,8 @@
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     if ([[NSUserDefaults standardUserDefaults] objectForKey: @"scores mode:1 diff:1"] == nil) {
      
-        NSArray* highScores = [NSArray arrayWithObjects: [NSNumber numberWithInt: 9801], [NSNumber numberWithInt: 5625], [NSNumber numberWithInt: 1728], [NSNumber numberWithInt: 1089], [NSNumber numberWithInt: 1], nil];
-        NSArray* nameList = [NSArray arrayWithObjects: [NSString stringWithFormat: @"Peter Wiggin"], [NSString stringWithFormat: @"Mr Rock"], [NSString stringWithFormat: @"Voldemort"], [NSString stringWithFormat: @"Ender"], [NSString stringWithFormat: @"Mr Steel"], nil];
+        NSArray* highScores = [NSArray arrayWithObjects: [NSNumber numberWithInt: 0], [NSNumber numberWithInt: 0], [NSNumber numberWithInt: 0], [NSNumber numberWithInt: 0], [NSNumber numberWithInt: 0], nil];
+        NSArray* nameList = [NSArray arrayWithObjects: [NSString stringWithFormat: @"???"], [NSString stringWithFormat: @"???"], [NSString stringWithFormat: @"???"], [NSString stringWithFormat: @"???"], [NSString stringWithFormat: @"???"], nil];
         
         for (int diff = 1; diff<4;diff++){
             for (int mode = 1; mode<3;mode++){
