04217c498899461b6cd2f18ef5e4a4636a2a84fc
kateaplin <kaplin@g.hmc.edu>
2013-06-28 13:53:46 -0700

    Count up score

--- CircleCircleRevolution/CircleCircleRevolution/GameScoreView.m
+++ CircleCircleRevolution/CircleCircleRevolution/GameScoreView.m
@@ -10,6 +10,10 @@
 #import "Config.h"
 
 @implementation GameScoreView
+{
+    int endValue;
+    double delta;
+}
 
 - (id)initWithFrame:(CGRect)frame
 {
@@ -41,6 +45,46 @@
     NSString* scoreString = [NSString stringWithFormat:@"%d", score];
     scoreLabel.text = scoreString;
 }
+
+-(void) updateValueBy:(NSNumber*)valueDelta
+{
+    // update property
+    value += [valueDelta intValue];
+    [self updateScore:value];
+    // are we at the end value? - if count up, else count down
+    if ([valueDelta intValue] >0){
+        if (value >endValue){
+            value = endValue;
+            [self updateScore:value];
+            return;
+        }
+    }
+    // if not, do it again
+    [self performSelector:@selector(updateValueBy:) withObject:valueDelta afterDelay:delta];
+}
+
+//count to a given value
+-(void) countTo:(int)to withDuration:(float)t
+{
+    //time for animation
+    delta = t/(abs(to-value)+1);
+    delta *= 25;
+    if (delta < 0.02) delta = 0.02;
+    
+    // set end value
+    endValue = to;
+    
+    // cancel previous stuff
+    [NSObject cancelPreviousPerformRequestsWithTarget:self];
+    
+    // detect which way counting goes
+    if (to-value>0){
+        //count up
+        [self updateValueBy:@25];
+    }
+}
+
+
 
 /*
 // Only override drawRect: if you perform custom drawing.
